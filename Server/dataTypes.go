d337fe66502551b01c3f08914a5d3653b2c32131
FreddieMatherSmartDCSIT <124710637+FreddieMatherSmartDCSIT@users.noreply.github.com>
2024-11-15 14:58:24 +0000

    Cdsopv 66 create wait group completion handler struct (#33)
    * CDSOPV-66 Tests for WaitGroupCompletionHandler
    
    * CDSOPV-66 Implement WaitGroupCompletionHandler
    
    * CDSOPV-66 update class diagram
    
    * CDSOPV-66 update struct desc and add error to Complete if isComplete already true

--- Server/dataTypes.go
+++ Server/dataTypes.go
@@ -1,12 +1,73 @@
 package Server
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 // CompletionHandler is an interface for handling actions upon task completion.
 // The Complete method is called when a task finishes, taking in the resulting data
 // and an error, if one occurred, and returns an error if the handling fails.
 type CompletionHandler interface {
 	Complete(data any, err error) error
+}
+
+type WaitGroupCompletionHandler struct {
+	// WaitGroupCompletionHandler is a struct that provides a way to wait for a task to complete
+	// It has the following fields:
+	// 1. wg: *sync.WaitGroup. A pointer to a WaitGroup that will be used to wait for the task to complete
+	// 2. data: any. The data that will be returned when the task completes
+	// 3. err: error. The error that will be returned when the task completes
+	// 4. isComplete: bool. A flag that indicates whether the task has completed
+	wg         *sync.WaitGroup
+	data       any
+	err        error
+	isComplete bool
+}
+
+func (w *WaitGroupCompletionHandler) Complete(data any, err error) error {
+	// Complete is a method that will be called when the task completes
+	// It will set the data and error fields and decrement the WaitGroup
+	// It returns an error if the WaitGroup is nil
+	if w.wg == nil {
+		return errors.New("WaitGroup not set")
+	}
+	if w.isComplete {
+		return errors.New("task already complete")
+	}
+	w.data = data
+	w.err = err
+	w.isComplete = true
+	w.wg.Done()
+	return nil
+}
+
+func (w *WaitGroupCompletionHandler) GetData() (any, error) {
+	// GetData is a method that returns the data field
+	if !w.isComplete {
+		return nil, errors.New("task not complete")
+	}
+	return w.data, nil
+}
+
+func (w *WaitGroupCompletionHandler) GetError() (error, error) {
+	// GetError is a method that returns the error field
+	// It returns an error if the task is not complete
+	if !w.isComplete {
+		return nil, errors.New("task not complete")
+	}
+	return w.err, nil
+}
+
+func NewWaitGroupCompletionHandler(wg *sync.WaitGroup) *WaitGroupCompletionHandler {
+	// NewWaitGroupCompletionHandler is a function that creates a new WaitGroupCompletionHandler
+	// It takes in a pointer to a WaitGroup and returns a pointer to a WaitGroupCompletionHandler
+	// It adds 1 to the WaitGroup
+	wg.Add(1)
+	return &WaitGroupCompletionHandler{
+		wg:         wg,
+		isComplete: false,
+	}
 }
 
 // AppData is a struct that holds data and a CompletionHandler for a task.
--- Server/dataTypes_test.go
+++ Server/dataTypes_test.go
@@ -2,6 +2,7 @@ package Server
 
 import (
 	"errors"
+	"sync"
 	"testing"
 )
 
@@ -57,6 +58,112 @@ func TestCompletionHandler(t *testing.T) {
 			t.Errorf("Expected error '%v', got '%v'", testError, handler.ErrorReceived)
 		}
 		// Type assertion to check if handler implements CompletionHandler
+		_, ok := interface{}(handler).(CompletionHandler)
+		if !ok {
+			t.Errorf("Expected handler to implement CompletionHandler interface")
+		}
+	})
+}
+
+// Tests for WaitGroupCompletionHandler
+func TestWaitGroupCompletionHandler(t *testing.T) {
+	t.Run("Complete", func(t *testing.T) {
+		wg := &sync.WaitGroup{}
+		handler := WaitGroupCompletionHandler{}
+		// Test when wg is nil
+		err := handler.Complete(nil, nil)
+		if err == nil {
+			t.Errorf("Expected error from Complete, got nil")
+		}
+		// Test when wg is set
+		wg.Add(1)
+		handler.wg = wg
+		// Test when task is already complete
+		handler.isComplete = true
+		err = handler.Complete(nil, nil)
+		if err == nil {
+			t.Errorf("Expected error from Complete, got nil")
+		}
+		// Test when task is not complete
+		handler.isComplete = false
+
+		testData := "test data"
+		err = handler.Complete(testData, nil)
+
+		if err != nil {
+			t.Errorf("Expected no error from Complete, got %v", err)
+		}
+		if handler.data != testData {
+			t.Errorf("Expected data to be '%v', got '%v'", testData, handler.data)
+		}
+		if handler.err != nil {
+			t.Errorf("Expected err to be nil, got %v", handler.err)
+		}
+		if !handler.isComplete {
+			t.Errorf("Expected isComplete to be true, got false")
+		}
+		wg.Wait()
+	})
+	t.Run("GetData", func(t *testing.T) {
+		handler := WaitGroupCompletionHandler{}
+		// Test when task is not complete
+		_, err := handler.GetData()
+		if err == nil {
+			t.Errorf("Expected error from GetData, got nil")
+		}
+		// Test when task is complete
+		handler.isComplete = true
+		testData := "test data"
+		handler.data = testData
+
+		data, err := handler.GetData()
+
+		if err != nil {
+			t.Errorf("Expected no error from GetData, got %v", err)
+		}
+		if data != testData {
+			t.Errorf("Expected data to be '%v', got '%v'", testData, data)
+		}
+	})
+	t.Run("GetError", func(t *testing.T) {
+		handler := WaitGroupCompletionHandler{}
+		// Test when task is not complete
+		_, err := handler.GetError()
+		if err == nil {
+			t.Errorf("Expected error from GetError, got nil")
+		}
+		// Test when task is complete
+		handler.isComplete = true
+		testError := errors.New("test error")
+		handler.err = testError
+
+		err, err2 := handler.GetError()
+
+		if err != testError {
+			t.Errorf("Expected error to be '%v', got '%v'", testError, err)
+		}
+		if err2 != nil {
+			t.Errorf("Expected no error from GetError, got %v", err2)
+		}
+	})
+	t.Run("NewWaitGroupCompletionHandler", func(t *testing.T) {
+		wg := &sync.WaitGroup{}
+
+		handler := NewWaitGroupCompletionHandler(wg)
+
+		if handler.wg != wg {
+			t.Errorf("Expected wg to be '%v', got '%v'", wg, handler.wg)
+		}
+		if handler.isComplete {
+			t.Errorf("Expected isComplete to be false, got true")
+		}
+		// Test for panic in WaitGroup.Done to make sure something has been added
+		// to the WaitGroup
+		wg.Done()
+	})
+	t.Run("ImplementCompletionHandler", func(t *testing.T) {
+		handler := &WaitGroupCompletionHandler{}
+
 		_, ok := interface{}(handler).(CompletionHandler)
 		if !ok {
 			t.Errorf("Expected handler to implement CompletionHandler interface")
