05ddb52705f5ea657e1f1cd0a7bd13ec962a4083
FreddieMatherSmartDCSIT <124710637+FreddieMatherSmartDCSIT@users.noreply.github.com>
2024-11-26 10:53:38 +0000

    Cdsopv 95 function to ingest json config file (#52)
    * CDSOPV-95 Create tests of ReadConfigJSON
    
    * CDSOPV-95 Implement ReadConfigJSON

--- Server/utils.go
+++ Server/utils.go
@@ -1,6 +1,9 @@
 package Server
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"os"
+)
 
 // convertBytesToMap is a function that converts a byte slice to a map.
 // It takes in a byte slice and returns a map[string]any and an error.
@@ -22,4 +25,23 @@ func convertBytesToArray(data []byte) ([]any, error) {
 		return nil, err
 	}
 	return jsonData, nil
+}
+
+// ReadConfigJSON is a function that reads a JSON file and returns a map.
+//
+// It takes as args:
+//
+// 1. path: string. The path to the JSON file.
+//
+// It returns:
+//
+// 1. map[string]any. The map that was read from the JSON file.
+//
+// 2. error. An error if ingestion fails.
+func ReadConfigJSON(path string) (map[string]any, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return convertBytesToMap(data)
 }
--- Server/utils_test.go
+++ Server/utils_test.go
@@ -1,6 +1,7 @@
 package Server
 
 import (
+	"os"
 	"reflect"
 	"testing"
 )
@@ -50,6 +51,54 @@ func TestConvertBytesToArray(t *testing.T) {
 	// Tests case where JSON is not valid
 	data = []byte(`["value"`)
 	_, err = convertBytesToArray(data)
+	if err == nil {
+		t.Errorf("Expected error")
+	}
+}
+
+// TestReadConfigJSON is a function that tests the ReadConfigJSON function.
+func TestReadConfigJSON(t *testing.T) {
+	// Tests case where file is read successfully
+	tmpFile1, err := os.CreateTemp("", "test.json")
+	if err != nil {
+		t.Fatalf("Error: %s", err)
+	}
+	defer os.Remove(tmpFile1.Name())
+	data := []byte(`{"key":"value"}`)
+	err = os.WriteFile(tmpFile1.Name(), data, 0644)
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+	expected := map[string]any{"key": "value"}
+	actual, err := ReadConfigJSON(tmpFile1.Name())
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected: %v, Actual: %v", expected, actual)
+	}
+	// Tests case where file is not found
+	tmpFile2, err := os.CreateTemp("", "notfound.json")
+	if err != nil {
+		t.Fatalf("Error: %s", err)
+	}
+	defer os.Remove(tmpFile2.Name())
+	_, err = ReadConfigJSON(tmpFile2.Name())
+	if err == nil {
+		t.Errorf("Expected error")
+	}
+	// Tests case where file is not valid
+	tmpFile3, err := os.CreateTemp("", "invalid.json")
+	if err != nil {
+		t.Fatalf("Error: %s", err)
+	}
+	defer os.Remove(tmpFile3.Name())
+	data = []byte(`{"key":"value"`)
+	err = os.WriteFile(tmpFile3.Name(), data, 0644)
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+	_, err = ReadConfigJSON(tmpFile3.Name())
 	if err == nil {
 		t.Errorf("Expected error")
 	}
