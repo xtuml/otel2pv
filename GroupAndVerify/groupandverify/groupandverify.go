cd8644c0d9dbfbd0302c9e4e894e9ab90ff9e4a9
FreddieMatherSmartDCSIT <124710637+FreddieMatherSmartDCSIT@users.noreply.github.com>
2024-12-11 13:13:19 +0000

    Cdsopv 117 create outgoing data struct (#68)
    * CDSOPV-117 create OutgoingData struct
    
    * CDSOPV-117 created IncomingData struct

--- GroupAndVerify/groupandverify/groupandverify.go
+++ GroupAndVerify/groupandverify/groupandverify.go
@@ -19,3 +19,40 @@ type GroupApply struct {
 	IdentifyingField       string
 	ValueOfIdentifyingField string
 }
+
+// OutgoingData that is used to hold the outgoing data from the GroupAndVerify
+// component. It has the following fields:
+// 1. NodeId: string. It is the identifier of the node.
+//
+// 2. OrderedChildIds: []string. It is the list of identifiers of the children of the node, in order
+// of occurence.
+//
+// 3. AppJSON: map[string]any. It is the JSON data that is to be sent to the next stage.
+type OutgoingData struct {
+	NodeId          string
+	OrderedChildIds []string
+	AppJSON         map[string]any
+}
+
+// IncomingData is a struct that is used to hold the incoming data from the previous stage
+// in the GroupAndVerify component. It has the following fields:
+// 1. NodeId: string. It is the identifier of the node.
+//
+// 2. ParentId: string. It is the identifier of the parent of the node.
+//
+// 3. ChildIds: []string. It is the list of identifiers of the children of the node.
+//
+// 4. NodeType: string. It is the type of the node. This can be used to identify if
+// bidirectional confirmation is required. Optional.
+//
+// 5. Timestamp: int. It is the timestamp of the node. Optional.
+//
+// 6. AppJSON: map[string]any. It is the JSON data that is to be processed and sent on.
+type IncomingData struct {
+	NodeId    string
+	ParentId  string
+	ChildIds  []string
+	NodeType  string
+	Timestamp int
+	AppJSON  map[string]any
+}
